0ec1625192769e572f42e154e0a213d5c38e4de2
David Barratt <david@davidwbarratt.com>
2014-02-22 12:55:34 -0500

    Fix the placement of the timestamp.

--- web/wp-content/themes/davidwbarratt/page.php
+++ web/wp-content/themes/davidwbarratt/page.php
@@ -3,9 +3,7 @@
 	<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 		<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 			<h2 class="entry-title"><?php the_title(); ?></h2>
-				<div class="hr"></div>
-				<div class="post-meta">
-				</div>
+		  <div class="hr"></div>
 			<div class="entry-content">
 				<?php the_content(); ?>
 				<?php edit_post_link( __( 'Edit', 'davidwbarratt' ), '<span class="edit-link">', '</span>' ); ?>
--- web/wp-content/themes/davidwbarratt/single.php
+++ web/wp-content/themes/davidwbarratt/single.php
@@ -3,12 +3,12 @@
 		<?php while ( have_posts() ) : the_post(); ?>
 			<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 				<h2 class="entry-title"><?php the_title(); ?></h2>
-				<div class="hr"></div>
 				<div class="post-meta">
 					<div class="post-date">
-						<?php print get_the_date('F j Y'); ?> at <?php print get_the_date('g:ia'); ?>
+						<?php print get_the_date('F j Y'); ?>
 					</div>
 				</div>
+				<div class="hr"></div>
 				<div class="entry-content">
 					<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'davidwbarratt' ) ); ?>
 					<?php edit_post_link( __( 'Edit', 'davidwbarratt' ), '<span class="edit-link">', '</span>' ); ?>
