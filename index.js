cbf5cc0e961584124855b13a37eb4a8191f655c0
David Barratt <david@davidwbarratt.com>
2020-12-26 19:22:56 -0500

    Make the worker better

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ import { parse } from 'cookie';
 
 const METHODS = new Set(['HEAD', 'GET']);
 
+function headResponse(original) {
+  const response = new Response('', origin);
+  return response;
+}
+
+function modifyResponse(original) {
+  // Recreate the response so we can modify the headers
+  const response = new Response(original.body, original)
+
+  // Set CORS headers
+  response.headers.set('Access-Control-Allow-Origin', '*');
+
+  return response;
+}
+
 /**
  * Respond with hello worker text
  * @param {Request} request
@@ -11,7 +26,8 @@ async function handleRequest(event) {
 
   // Bypass for methods other than HEAD & GET
   if (!METHODS.has(request.method)) {
-    return fetch(request);
+    const originResponse = await fetch(request);
+    return modifyResponse(originResponse);
   }
 
   // Bypass on session cookie.
@@ -21,40 +37,39 @@ async function handleRequest(event) {
     const hasSessionCookie = !!Object.keys(cookies).find((name) => name.startsWith('SSESS'));
 
     if (hasSessionCookie) {
-      return fetch(request);
+      const originResponse = await fetch(request);
+      return modifyResponse(originResponse);
     }
   }
 
   const cache = caches.default;
-  const cachedResponse = await cache.match(request);
+  const cachedResponse = await cache.match(request, {
+    ignoreMethod: true,
+  });
 
   // Cache HIT
   if (cachedResponse) {
-    return cachedResponse;
+    return request.method === 'HEAD' ? headResponse(cachedResponse) : cachedResponse;
   }
 
   // Cache MISS
   const originResponse = await fetch(request);
-
-  // Recreate the response so we can modify the headers
-  const response = new Response(originResponse.body, originResponse)
-
-  // Set CORS headers
-  response.headers.set('Access-Control-Allow-Origin', '*');
-
-  // Bypass on static content.
-  if (response.headers.get('CF-Cache-Status') !== 'DYNAMIC') {
-    return response;
-  }
+  const response = modifyResponse(originResponse);
 
   // Bypass on Set-Cookie
   if (response.headers.has('Set-Cookie')) {
     return response;
   }
 
+  if (response.headers.get('CF-Cache-Status') === 'DYNAMIC' && response.status === 200) {
+    response.headers.set('Cache-Control', 'public, max-age=60, s-maxage=31536000');
+  } else {
+    response.headers.set('Cache-Control', 'public, max-age=2628000, s-maxage=31536000');
+  }
+
   event.waitUntil(cache.put(request, response.clone()));
 
-  return response;
+  return request.method === 'HEAD' ? headResponse(response) : response;;
 }
 
 // eslint-disable-next-line no-restricted-globals
